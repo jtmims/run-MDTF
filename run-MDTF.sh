ba399cd037e46eb6616844a7d930db73f562fc95
Jacob Mims <122570226+jtmims@users.noreply.github.com>
2025-02-27 10:22:25 -0500

    launch new config scripts

--- run-MDTF.sh
+++ run-MDTF.sh
@@ -11,9 +11,9 @@ catbuilddir=/home/Jacob.Mims/CatalogBuilder/
 
 echo $run_dir
 
-#TEST: /archive/jpk/fre/FMS2024.02_OM5_20240819/CM4.5v01_om5b06_piC_noBLING_xrefine_test4/gfdl.ncrc5-intel23-prod-openmp/pp/
+#TEST: /archive/jpk/fre/FMS2024.02_OM5_20240819/CM4.5v01_om5b06_piC_noBLING_xrefine_test4/gfdl.ncrc5-intel23-prod-openmp/pp/ starts 0001
 
-#TEST 2: /archive/djp/am5/am5f7b12r1/c96L65_am5f7b12r1_amip/gfdl.ncrc5-intel23-classic-prod-openmp/pp/
+#TEST 2: /archive/djp/am5/am5f7b12r1/c96L65_am5f7b12r1_amip/gfdl.ncrc5-intel23-classic-prod-openmp/pp/ starts 1990
 
 usage() {
    echo "USAGE: run-mdtf.sh /path/to/pp/dir/pp out_dir/mdtf startyr endyr"   
@@ -90,40 +90,10 @@ echo "edited file $f"
 #generate config files
 python $run_dir/scripts/gen_config.py $outdir/
 
-exit 0 
-declare -a files=(
-[0]="atmos_cmip_config.jsonc"
-[1]="ice_config.jsonc"
-)
-if [ $startyr -le 2003 ] && [ $endyr -ge 2014 ]; then
-   files=("${files[@]}"  "atmos_cmip_ffb.jsonc")
-fi
-
-# edit config files
-for f in "${files[@]}" ; do
-   cp $run_dir/config/$f $outdir/$f
-   config='"DATA_CATALOG": "",'
-   config_edit='"DATA_CATALOG": "'"${cat}"'",'
-   sed -i "s|$config|$config_edit|ig" $outdir/$f
-   config='"WORK_DIR": "",'
-   config_edit='"WORK_DIR": "'"${outdir}"'",'
-   sed -i "s|$config|$config_edit|ig" $outdir/$f
-   # handle atmos_cmip PODs
-   config='"startdate": "",'
-   config_edit='"startdate": "'"${startyr}"'",'
-   sed -i "s|$config|$config_edit|ig" $outdir/$f
-   config='"enddate": ""'
-   config_edit='"enddate": "'"${endyr}"'"'
-   sed -i "s|$config|$config_edit|ig" $outdir/$f
-   echo "edited file $f"
-done
-
 # launch the mdtf with the config files
-for f in "${files[@]}" ; do
-   if [ -f $outdir/$f ]; then
-      echo "launching MDTF with $f"
-      "$mdtf_dir"/mdtf -f $outdir/$f
-   fi
+for f in $(ls ${outdir}/config) ; do
+   echo "launching MDTF with $f"
+   "$mdtf_dir"/mdtf -f $outdir/config/$f
 done
 
 # consolidate into a single output folder
