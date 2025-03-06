11eb659266da18b50b605c88137653d685987445
Jacob Mims <122570226+jtmims@users.noreply.github.com>
2025-03-06 15:51:08 -0500

    fix comments and print in scripts/req_var_search.py

--- scripts/req_var_search.py
+++ scripts/req_var_search.py
@@ -16,12 +16,14 @@ pod_passed = {}
 runnable_pods = {}
 requested_pods=[]
 
+# grab requested PODs from cli
 if len(sys.argv) > 4:
     for i in range (4, len(sys.argv)):
         requested_pods.append(sys.argv[i])
+if len(requested_pods) > 0:
+    print(f"LOG: user requested PODs {requested_pods}")
 
-print(f"LOG: user requested PODs {requested_pods}")
-
+#search catalog for required variables, if not there throw out POD
 for p in req_vars:
     if len(requested_pods) > 0 and p not in requested_pods:
         continue
@@ -39,5 +41,6 @@ for p in req_vars:
         del req_vars[p]['vars']
         runnable_pods[p] = req_vars[p]
 
+# write out list of PODs to add to config files
 with open(sys.argv[3]+"runnable_pods.json", "w") as f:
     f.write(json.dumps(runnable_pods, indent=2))
